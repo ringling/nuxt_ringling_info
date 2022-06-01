09058b84b498cdd40bb47dbf1165bb1b62bdafc2
Thomas Ringling <thomas@ringling.info>
2022-06-01 14:12:57 +0200

    Create index page

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="h-screen w-screen bg-cover bg-[left_-12rem_top] md:bg-left-top bg-[url(/img/home-bg-1.jpg)]">
+    <div class="container mx-auto p-4 ">
+      <main class="container">
+        <p class="alert alert-info" role="alert"></p>
+        <p class="alert alert-danger" role="alert"></p>
+        <h1 class="text-black  text-4xl xl:text-6xl font-bold text-right mt-12 mr-12">Creative Technologist &amp;<br>
+          Software Enthusiast</h1>
+
+        <section class="m-12 text-right">
+          <p class="mb-2 text-xl xl:text-2xl">A little about who I am</p>
+          <p class="mb-2 text-xl xl:text-2xl">What I am doing right now</p>
+          <p class="mb-2 text-xl xl:text-2xl">And what I have been doing</p>
+        </section>
+
+        <div class="cloud top-0 left-0 absolute h-full w-full" style="background: url(/img/cloud.png)"></div>
+      </main>
+    </div>
+  </div>
+</template>
