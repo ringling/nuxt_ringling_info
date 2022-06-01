b6f135e781120e1aaf5b079ee760840bcce2374a
Thomas Ringling <thomas@ringling.info>
2022-06-01 15:05:48 +0200

    Fix header and add about and now page

---
+++ pages/about.vue
@@ -0,0 +1,386 @@
+<template>
+    <div class="">
+        <div class="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-gray-900/10 dark:border-gray-50/[0.06] bg-white supports-backdrop-blur:bg-white/95 dark:bg-gray-900/75">
+            <div
+                class="max-w-8xl mx-auto dark:bg-gray-900/75 border-b border-gray-900/10 lg:px-8 dark:border-gray-300/10">
+                <div class="py-4  mx-4 lg:mx-0">
+                    <div class="relative flex items-center">
+                        <a class="mr-3 flex-none w-[2.0625rem] overflow-hidden md:w-auto" href="/">
+                            <span class="sr-only">Thomas Ringling home page</span>
+                            <img class="w-auto h-8" src="/img/logo.svg">
+                        </a>
+                     
+                        <div class="relative flex items-center mr-8 ml-8">
+                            <nav class="text-sm leading-6 font-semibold text-gray-700 dark:text-gray-200">
+                                <ul class="flex space-x-8">
+                                    <li>
+                                        <NuxtLink class="hover:text-sky-500 dark:hover:text-sky-400"
+                                            style="z-index: 1000" to="/">Home</NuxtLink>
+                                    </li>
+                                    <li>
+                                        <NuxtLink class="hover:text-sky-500 dark:hover:text-sky-400"
+                                            style="z-index: 1000" to="/now">Now</NuxtLink>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container mx-auto p-4 ">
+
+            <div>
+                <h1 class="text-black text-4xl font-bold my-4">About</h1>
+
+
+                <h2 class="text-black text-3xl font-bold my-4">Introduction</h2>
+                <section class="m-8 flex flex-wrap">
+
+                    <div>
+
+                        <p class="mt-6 text-lg text-gray-600 dark:text-gray-400">
+                            Living north of Copenhagen with my wife and my daughter.
+                        </p>
+
+                        <p class="mt-2 text-lg text-gray-600 dark:text-gray-400">
+                            Lifetime learner and polyglot software craftsman with a great love for physical training,
+                            music and nature.
+                        </p>
+
+                        <p class="mt-2 text-lg text-gray-600 dark:text-gray-400">
+                            A long experience implementing and teaching agile methodologies and principles.
+                        </p>
+
+                        <p class="mt-2 text-lg text-gray-600 dark:text-gray-400">
+                            With over 20 years of experience within software development, I span a variety of different
+                            technologies, paradigms and programming languages.
+                        </p>
+
+                        <p class="mt-2 text-lg text-gray-600 dark:text-gray-400">
+                            I have had many roles within and outside IT, such as developer, coach, teacher, architect,
+                            project manager, scrum master, product owner, team lead etc.
+                        </p>
+
+                        <p class="mt-2 text-lg text-gray-600 dark:text-gray-400">
+                            As I still do software development, I keep up with new technologies and hence thrive in
+                            environments and cultures who value competences, innovation and creativity.
+                        </p>
+                    </div>
+
+                </section>
+
+
+                <br>
+
+
+                <h2 class="text-black text-3xl font-bold my-4">Education</h2>
+                <div class="grid grid-cols- gap-6 lg:gap-8 sm:grid-cols-2 lg:grid-cols-3 mb-8">
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <blockquote class="mt-6 text-gray-700 dark:text-gray-300">
+                            <p>2002 - 2005</p>
+                        </blockquote>
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Diploma in Information Technology</h3>
+                                <div class="mt-0.5">IT-University Copenhagen</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <blockquote class="mt-6 text-gray-700 dark:text-gray-300">
+                            <p>1998 - 2000</p>
+                        </blockquote>
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Datamatician</h3>
+                                <div class="mt-0.5">Lyngby Uddannelsescenter</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <blockquote class="mt-6 text-gray-700 dark:text-gray-300">
+                            <p>1988 - 1991</p>
+                        </blockquote>
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>High school</h3>
+                                <div class="mt-0.5">Deutsches Gymnasium für Nordschleswig</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                </div>
+
+                <h2 class="text-black text-3xl font-bold my-4">Languages</h2>
+
+                <div class="grid grid-cols- gap-6 lg:gap-8 sm:grid-cols-2 lg:grid-cols-3 mb-8">
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Danish</h3>
+                                <div class="mt-0.5">Native proficiency</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>German</h3>
+                                <div class="mt-0.5">Native proficiency</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>English</h3>
+                                <div class="mt-0.5">Full professional proficiency</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Japanese</h3>
+                                <div class="mt-0.5">Elementary proficiency</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                </div>
+
+
+
+                <h2 class="text-black text-3xl font-bold my-4">Courses</h2>
+
+                <div class="grid grid-cols- gap-6 lg:gap-8 sm:grid-cols-2 lg:grid-cols-3 mb-8">
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Product Owner 360</h3>
+                                <div class="mt-0.5">2019 - GoAgile</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Lead for Greatness</h3>
+                                <div class="mt-0.5">2018 - GoAgile</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Angular 2.0</h3>
+                                <div class="mt-0.5">2016 - Trifork</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Stillwater Leadership</h3>
+                                <div class="mt-0.5">2016 - Stillwater</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Graphical Facilitation</h3>
+                                <div class="mt-0.5">2016 - Future Factory</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Agile Project Leadership</h3>
+                                <div class="mt-0.5">2012 - GoAgile</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                </div>
+
+
+                <h2 class="text-black text-3xl font-bold my-4">Certifications</h2>
+
+
+                <div class="grid grid-cols-1 gap-6 lg:gap-8 sm:grid-cols-2 lg:grid-cols-3 mb-8">
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Introduction to Quantum Computing for Everyone</h3>
+                                <div class="mt-0.5">2021 - edX / ChicagoX</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>QTM1x: The Quantum Internet and Quantum Computers: How Will They Change the World?
+                                </h3>
+                                <div class="mt-0.5">2021 - edX / DelftX</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Certified LeSS practitioner</h3>
+                                <div class="mt-0.5">2021 - LeSS</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Certified SAfe agilist</h3>
+                                <div class="mt-0.5">2016 - SAFe</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Professional Scrum Master</h3>
+                                <div class="mt-0.5">2013 - Scrum.org</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Agile Foundation Leadership Focus</h3>
+                                <div class="mt-0.5">2012 - DSDM</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                </div>
+
+
+                <h2 class="text-black text-3xl font-bold my-4">Conferences</h2>
+
+
+                <div class="grid grid-cols- gap-6 lg:gap-8 sm:grid-cols-2 lg:grid-cols-3 mb-8">
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Agile Coach Camp (co-organizer)</h3>
+                                <div class="mt-0.5">2019 - Denmark</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Elixir Conf Europe</h3>
+                                <div class="mt-0.5">2017 - Barcelona</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Elixir Conf Europe</h3>
+                                <div class="mt-0.5">2016 - Berlin</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Agile Coach Camp (co-organizer)</h3>
+                                <div class="mt-0.5">2016 - Denmark</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Agile Coach Camp</h3>
+                                <div class="mt-0.5">2016 - Denmark</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>Agile Coach Camp</h3>
+                                <div class="mt-0.5">2015 - Denmark</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                    <figure
+                        class="relative flex flex-col-reverse bg-gray-50 rounded-lg p-6 dark:bg-gray-800 dark:highlight-white/5">
+                        <figcaption class="flex items-center space-x-4">
+                            <div class="flex-auto">
+                                <h3>XP Days</h3>
+                                <div class="mt-0.5">2015 - Benelux</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
--- pages/now.vue
+++ pages/now.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="h-screen w-screen bg-cover bg-[left_-12rem_top] md:bg-left-top bg-[url(/img/home-bg-1.jpg)]">
-
-    <div class="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-gray-900/10 dark:border-gray-50/[0.06] supports-backdrop-blur:bg-white/95 dark:bg-gray-900/75">
+    <div class="">
+                <div class="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-gray-900/10 dark:border-gray-50/[0.06] bg-white supports-backdrop-blur:bg-white/95 dark:bg-gray-900/75">
             <div
                 class="max-w-8xl mx-auto dark:bg-gray-900/75 border-b border-gray-900/10 lg:px-8 dark:border-gray-300/10">
                 <div class="py-4  mx-4 lg:mx-0">
@@ -10,17 +9,18 @@
                             <span class="sr-only">Thomas Ringling home page</span>
                             <img class="w-auto h-8" src="/img/logo.svg">
                         </a>
-                     
+                       
                         <div class="relative flex items-center mr-8 ml-8">
                             <nav class="text-sm leading-6 font-semibold text-gray-700 dark:text-gray-200">
                                 <ul class="flex space-x-8">
                                     <li>
                                         <NuxtLink class="hover:text-sky-500 dark:hover:text-sky-400"
-                                            style="z-index: 1000" to="/about">About</NuxtLink>
+                                            style="z-index: 1000" to="/">Home</NuxtLink>
                                     </li>
+
                                     <li>
                                         <NuxtLink class="hover:text-sky-500 dark:hover:text-sky-400"
-                                            style="z-index: 1000" to="/now">Now</NuxtLink>
+                                            style="z-index: 1000" to="/about">About</NuxtLink>
                                     </li>
                                 </ul>
                             </nav>
@@ -30,21 +30,11 @@
             </div>
         </div>
 
-    <div class="container mx-auto p-4 ">
-      <main class="container">
-        <p class="alert alert-info" role="alert"></p>
-        <p class="alert alert-danger" role="alert"></p>
-        <h1 class="text-black  text-4xl xl:text-6xl font-bold text-right mt-12 mr-12">Creative Technologist &amp;<br>
-          Software Enthusiast</h1>
+        <div class="container mx-auto p-4 ">
 
-        <section class="m-12 text-right">
-          <p class="mb-2 text-xl xl:text-2xl">A little about who I am</p>
-          <p class="mb-2 text-xl xl:text-2xl">What I am doing right now</p>
-          <p class="mb-2 text-xl xl:text-2xl">And what I have been doing</p>
-        </section>
-
-        <div class="cloud top-0 left-0 absolute h-full w-full" style="background: url(/img/cloud.png)"></div>
-      </main>
+            <div>
+                <h1 class="text-black text-4xl font-bold my-4">Now</h1>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
