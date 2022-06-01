40631ca98199a3d94da9602660e503456c20d56c
Thomas Ringling <thomas@ringling.info>
2022-06-01 12:30:51 +0200

    Tailwind stylesheet with example

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  content: [],
+  content: [
+    `components/**/*.{vue,js,ts}`,
+    `layouts/**/*.vue`,
+    `pages/**/*.vue`,
+    `app.vue`,
+    `plugins/**/*.{js,ts}`,
+    `nuxt.config.{js,ts}`,
+  ],
   theme: {
     extend: {},
   },
