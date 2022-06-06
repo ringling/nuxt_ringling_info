c9649993c14000d192422609f1c0e0ba27539e94
Thomas Ringling <thomas@ringling.info>
2022-06-06 19:21:21 +0200

    OFix async not ready bug

--- pages/cv.vue
+++ pages/cv.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="">
-    <Menu bgColor="bg-white" />
+	<div class="">
+		<Menu bgColor="bg-white" />
 
-    <div class="container mx-auto p-4 ">
-      <div>
-        <h1 class="m-4">CV</h1>
-      </div>
-    </div>
-   *{{data.cv.basics}}*
-  </div>
+		<div class="container mx-auto p-4 ">
+			<div>
+				<h1 class="m-4">CV</h1>
+			</div>
+			<div v-if="data">
+				<pre>
+				{{ data.cv }}
+				</pre>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup>
-    const { pending, data: data } = useLazyAsyncData('cv', () => $fetch('/api/cv'))
+const { pending, data: data } = await useLazyAsyncData('cv', () => $fetch('/api/cv'))
 </script>
--- server/api/cv.ts
+++ server/api/cv.ts
@@ -3,7 +3,7 @@ import { readFileSync } from 'fs';
 const file = readFileSync('server/api/data.json', 'utf-8');
 
 export default () => (
-    {
-        cv:  JSON.parse(file)
-    }
+  {
+    cv: JSON.parse(file)
+  }
 )
