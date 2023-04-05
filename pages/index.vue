a3955511bfed1f854d1a0ca7daa3d67f39e4532d
Thomas Ringling <github@ringling.info>
2023-04-05 09:09:30 +0200

    New background image

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen w-screen bg-cover bg-[left_-12rem_top] md:bg-left-top bg-[url(/img/home-bg-1.jpg)]">
+  <div class="h-screen w-screen bg-cover bg-[left_-12rem_top] md:bg-left-top bg-[url(/img/boy.png)]">
 
     <Menu />
 
