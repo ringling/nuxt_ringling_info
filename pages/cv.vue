1a336ad4d139191f5e09a222b5bbff752018f44f
Thomas Ringling <github@ringling.info>
2023-04-30 12:24:14 +0200

    Contact information

--- pages/cv.vue
+++ pages/cv.vue
@@ -5,11 +5,47 @@
 		<div class="container cv mx-auto p-4 ">
 			<div>
 				<h1 class="m-4">CV</h1>
-
 				<div class="flex justify-center">
 					<img src="/img/cv.jpg" class="lg:w-48 md:w-48 w-32 rounded-lg">
 				</div>
 
+				<h2 class="m-4">Contact</h2>
+				<div class="m-4 grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+					<div class="flex">
+						<svg width="25px" class="mr-2" height="25px" viewBox="0 0 24 24" fill="none"
+							xmlns="http://www.w3.org/2000/svg">
+							<path
+								d="M16 12C16 14.2091 14.2091 16 12 16C9.79086 16 8 14.2091 8 12C8 9.79086 9.79086 8 12 8C14.2091 8 16 9.79086 16 12ZM16 12V13.5C16 14.8807 17.1193 16 18.5 16V16C19.8807 16 21 14.8807 21 13.5V12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21H16"
+								stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+						</svg> <a href="mailto:cv@ringling.info">cv@ringling.info</a>
+					</div>
+
+					<div class="flex">
+						<svg width="25px" height="25px" class="mr-2" viewBox="0 -2 20 20" version="1.1"
+							xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+							<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+								<g id="Dribbble-Light-Preview" transform="translate(-60.000000, -7521.000000)" fill="#000000">
+									<g id="icons" transform="translate(56.000000, 160.000000)">
+										<path
+											d="M10.29,7377 C17.837,7377 21.965,7370.84365 21.965,7365.50546 C21.965,7365.33021 21.965,7365.15595 21.953,7364.98267 C22.756,7364.41163 23.449,7363.70276 24,7362.8915 C23.252,7363.21837 22.457,7363.433 21.644,7363.52751 C22.5,7363.02244 23.141,7362.2289 23.448,7361.2926 C22.642,7361.76321 21.761,7362.095 20.842,7362.27321 C19.288,7360.64674 16.689,7360.56798 15.036,7362.09796 C13.971,7363.08447 13.518,7364.55538 13.849,7365.95835 C10.55,7365.79492 7.476,7364.261 5.392,7361.73762 C4.303,7363.58363 4.86,7365.94457 6.663,7367.12996 C6.01,7367.11125 5.371,7366.93797 4.8,7366.62489 L4.8,7366.67608 C4.801,7368.5989 6.178,7370.2549 8.092,7370.63591 C7.488,7370.79836 6.854,7370.82199 6.24,7370.70483 C6.777,7372.35099 8.318,7373.47829 10.073,7373.51078 C8.62,7374.63513 6.825,7375.24554 4.977,7375.24358 C4.651,7375.24259 4.325,7375.22388 4,7375.18549 C5.877,7376.37088 8.06,7377 10.29,7376.99705"
+											id="twitter-[#154]">
+										</path>
+									</g>
+								</g>
+							</g>
+						</svg> <a href="https://www.linkedin.com/in/thomas-ringling-88698b/" target="blank">Thomas Ringling</a>
+					</div>
+
+					<div class="flex">
+						<svg width="25px" height="25px" class="mr-2" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+							<rect x="0" fill="none" width="20" height="20" />
+							<g>
+								<path
+									d="M2.5 18h3V6.9h-3V18zM4 2c-1 0-1.8.8-1.8 1.8S3 5.6 4 5.6s1.8-.8 1.8-1.8S5 2 4 2zm6.6 6.6V6.9h-3V18h3v-5.7c0-3.2 4.1-3.4 4.1 0V18h3v-6.8c0-5.4-5.7-5.2-7.1-2.6z" />
+							</g>
+						</svg> @thomasringling
+					</div>
+				</div>
 
 
 				<h2 class="m-4">Skills</h2>
