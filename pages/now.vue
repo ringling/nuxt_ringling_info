84538149375b362c10e5f66ecc2bafa6b4dc3776
Thomas Ringling <thomas@ringling.info>
2023-09-27 10:44:59 +0200

    Added ML book

--- pages/now.vue
+++ pages/now.vue
@@ -44,6 +44,7 @@
             <ul>
               <li class="grid grid-cols-1"> <b>Jytte fra Marketing er desværre gået for i dag</b> <em class="whitespace-nowrap">Morten Münster</em> </li>
               <li class="grid grid-cols-1"> <b>Viking</b> <em class="whitespace-nowrap">Jeanette Varberg</em> </li>
+              <li class="grid grid-cols-1"> <b>grokking Machine Learning</b> <em class="whitespace-nowrap">Luis G. Serrano</em> </li>
               <!-- <li class="grid grid-cols-1"> <b>A Column of Fire</b> <em class="whitespace-nowrap">Ken Follett</em> </li> -->
 
             </ul>
