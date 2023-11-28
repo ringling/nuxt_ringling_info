39cc94cb311857518e76b2043456f036936c743f
Thomas Ringling <thomas@ringling.info>
2023-11-28 11:35:02 +0100

    Gen AI course certificate

--- pages/now.vue
+++ pages/now.vue
@@ -15,7 +15,7 @@
         generated with
         Midjourney. Deployed via Github to DigitalOcean App Platform
       </div>
-      <div class="m-4 mb-8 text-xs">Last updated: November 18, 2023</div>
+      <div class="m-4 mb-8 text-xs">Last updated: November 28, 2023</div>
       <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
 
           <div class="block max-w p-6 bg-white markdown neu">
