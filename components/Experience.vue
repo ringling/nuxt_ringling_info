4bb3e0ebc2b72498f56494ad7584d7d134dfc175
Thomas Ringling <thomas@ringling.info>
2023-11-28 12:12:49 +0100

    Show technoligies, and updated tech on CV

--- components/Experience.vue
+++ components/Experience.vue
@@ -4,16 +4,11 @@
       <div class="flex-auto">
         <h3 class="mb-1 font-normal">{{ item.startDate }} - {{ item.endDate }}</h3>
         <h3 class="mb-1">{{ item.companyName }}</h3>
-        <div v-if="showDetails">{{ item.title }}</div>
-
+        <div>{{ item.title }}</div>
         <div class="my-2 normal-case" v-html="item.summary" v-if="showDetails"></div>
-        <!--
-
           <ul v-for="tech in item.technologies" v-if="showDetails">
             <li class="normal-case list-disc list-inside">{{ tech }}</li>
           </ul>
-        -->
-
       </div>
     </figcaption>
   </figure>
