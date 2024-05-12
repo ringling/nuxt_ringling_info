0eac5319c2f92388c51edf7277ea46d1e7473b64
Thomas Ringling <thomas@ringling.info>
2024-05-12 20:20:08 +0200

    Interests updated

--- pages/now.vue
+++ pages/now.vue
@@ -13,14 +13,14 @@ Team<template>
         generated with
         Midjourney. Deployed via Github to DigitalOcean App Platform
       </div>
-      <div class="m-4 mb-8 text-xs">Last updated: May 7, 2024</div>
+      <div class="m-4 mb-8 text-xs">Last updated: May 12, 2024</div>
       <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
         <div class="block max-w p-6 bg-white markdown neu">
           <img src="/img/porto_santo.jpg" class="lg:w-48 md:w-32 w-16 rounded-lg float-right" /><h5 class="mb-2 mt-4 text-2xl font-bold tracking-tight text-black">May 2024</h5>
           <p class="font-normal text-gray-700">
             <h3>Areas of interest</h3>
             <ul>
-              <li>Systems Thinking</li>
+              <li>LlamaIndex, RAG and ChromaDB</li>
               <li>BDD / Behave</li>
             </ul>
             <h3>Courses</h3>
