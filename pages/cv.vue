0bda470c9566b6cc0b6c655674910cf9bfabe1c3
Thomas Ringling <thomas@ringling.info>
2024-06-26 21:06:49 +0200

    showDetails via query param

--- pages/cv.vue
+++ pages/cv.vue
@@ -199,7 +199,7 @@
 
 				<div class=" grid grid-cols- gap-6 lg:gap-8 sm:grid-cols-2 lg:grid-cols-3 mb-4">
 					<Experience v-for="experience in (data.cv.experiences)" :item="experience" v-bind:key="experience"
-						:showDetails="showDetails" />
+						:showDetails="showDetails || route.query.showDetails=='true'" />
 				</div>
 
 				<h2 class="m-4">Technologies</h2>
