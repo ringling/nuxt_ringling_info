30ba4c9437a9ccc6c72d65521604b428fde644a8
Thomas Ringling <thomas@ringling.info>
2024-12-17 18:28:44 +0100

    New book

--- pages/now.vue
+++ pages/now.vue
@@ -13,7 +13,7 @@
         generated with
         Midjourney. Deployed via Github to DigitalOcean App Platform
       </div>
-      <div class="m-4 mb-8 text-xs">Last updated: December 8, 2024</div>
+      <div class="m-4 mb-8 text-xs">Last updated: December 17, 2024</div>
 
       <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
         <div class="block max-w p-6 bg-white markdown neu">
@@ -32,6 +32,7 @@
             <h3>Books</h3>
             <ul>
               <!-- <li class="grid grid-cols-1"> <b>The Consolation of Philosophy</b> <em class="whitespace-nowrap">Boethius</em></li> -->
+              <li class="grid grid-cols-1"> <b>It Doesn’t Have to Be Crazy at Work</b> <em class="whitespace-nowrap">Jason Fried & David Heinemeier Hansson</em></li>
               <li class="grid grid-cols-1"> <b>Vikingernes Helte</b> <em class="whitespace-nowrap">Niels Saxtorph</em></li>
               <li class="grid grid-cols-1"> <b>Build a Large Language Model (From Scratch)</b> <em class="whitespace-nowrap">Sebastian Raschka</em></li>
             </ul>
