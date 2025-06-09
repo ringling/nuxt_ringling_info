a17bf2c1d10efc4c6588120688450ab68b005c9a
Thomas Ringling <thomas.ringling@nestech.dk>
2025-06-09 12:27:42 +0200

    May 2025

--- pages/now.vue
+++ pages/now.vue
@@ -18,6 +18,23 @@
       <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
 
         <div class="block max-w p-6 bg-white markdown neu">
+          <div  class="bg-contain bg-no-repeat bg-center border bg-[url('/img/faroer.jpg')] lg:w-48 lg:h-48 md:w-32 md:h-32 h-16 w-16 rounded-lg float-right" />
+          <h5 class="mb-2 mt-4 text-2xl font-bold tracking-tight text-black">May 2025</h5>
+          <p class="font-normal text-gray-700">
+            <h3>Areas of interest</h3>
+            <ul>
+              <li>Raycast</li>
+              <li>Productivity</li>
+            </ul>
+
+            <h3>Books</h3>
+            <ul>
+              <li class="grid grid-cols-1"> <b>Berlin brænder</b> <em class="whitespace-nowrap">Tom Buk-Swienty</em> </li>
+            </ul>
+          </p>
+        </div>
+
+        <div class="block max-w p-6 bg-white markdown neu">
           <div  class="bg-contain bg-no-repeat bg-center border bg-[url('/img/europe_first.png')] lg:w-48 lg:h-48 md:w-32 md:h-32 h-16 w-16 rounded-lg float-right hover:bg-[url('/img/nhi.jpg')]" />
           <h5 class="mb-2 mt-4 text-2xl font-bold tracking-tight text-black">April 2025</h5>
           <p class="font-normal text-gray-700">
