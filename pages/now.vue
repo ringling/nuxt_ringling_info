c41b1d6b441be6de14e791474f43fd58bb16adba
Thomas Ringling <thomas.ringling@nestech.dk>
2025-09-14 09:42:45 +0200

    add august 2025

--- pages/now.vue
+++ pages/now.vue
@@ -13,9 +13,25 @@
         generated with
         Midjourney. Deployed on Hetzner as a Docker image
       </div>
-      <div class="m-4 mb-8 text-xs">Last updated: July 26, 2025</div>
+      <div class="m-4 mb-8 text-xs">Last updated: September 14, 2025</div>
 
       <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        
+        <div class="block max-w p-6 bg-white markdown neu">
+          <div  class="bg-contain bg-no-repeat bg-center border bg-[url('/img/olivia.png')] lg:w-48 lg:h-48 md:w-32 md:h-32 h-16 w-16 rounded-lg float-right" />
+          <h5 class="mb-2 mt-4 text-2xl font-bold tracking-tight text-black">August 2025</h5>
+          <div class="font-normal text-gray-700">
+            <h3>Areas of interest</h3>
+            <ul>
+              <li>Claude Code(subagents)</li>
+              <li>MCP servers</li>
+            </ul>
+            <h3>Books</h3>
+            <ul>
+              <li class="grid grid-cols-1"> <b>L'Heure des Prédateurs</b> <em class="whitespace-nowrap">Giuliano da Empoli</em> </li>
+            </ul>
+          </div>
+        </div>
 
         <div class="block max-w p-6 bg-white markdown neu">
           <div  class="bg-contain bg-no-repeat bg-center border bg-[url('/img/icehotel.jpg')] lg:w-48 lg:h-48 md:w-32 md:h-32 h-16 w-16 rounded-lg float-right" />
@@ -29,10 +45,9 @@
             </ul>
             <h3>Books</h3>
             <ul>
+              <li class="grid grid-cols-1"> <b>Essential GraphRAG</b> <em class="whitespace-nowrap">Tomaž Bratanič & Oskar Hane</em> </li>
               <li class="grid grid-cols-1"> <b>Adopting Elixir</b> <em class="whitespace-nowrap">Ben Marx, José Valim, Bruce Tate</em> </li>
               <li class="grid grid-cols-1"> <b>Genesis</b> <em class="whitespace-nowrap">Henry A. Kissinger, Eric Schmidt, Craig Mundie</em> </li>
-              <li class="grid grid-cols-1"> <b>Essential GraphRAG</b> <em class="whitespace-nowrap">Tomaž Bratanič & Oskar Hane</em> </li>
-              <li class="grid grid-cols-1"> <b>L'Heure des Prédateurs</b> <em class="whitespace-nowrap">Giuliano da Empoli</em> </li>
             </ul>
           </div>
         </div>
